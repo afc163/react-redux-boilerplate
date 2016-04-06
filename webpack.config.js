46c1778f3c8b10d271e0b43520ed3ee08cb40a3c
sorrycc <sorrycc@gmail.com>
2016-04-06 16:14:02 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,5 @@
 const webpack = require('atool-build/lib/webpack');
 
 module.exports = function(webpackConfig) {
-  webpackConfig.module.loaders.forEach(function(item) {
-    if (item.loader.indexOf('css?sourceMap')) {
-      item.loader = item.loader.replace('css?sourceMap',
-        'css?modules&sourceMap&localIdentName=[name]__[local]___[hash:base64:5]');
-    }
-  });
   return webpackConfig;
 };
