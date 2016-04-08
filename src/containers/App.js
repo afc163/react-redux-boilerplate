05f5d94db3cdbc7b22f4308a06f100dcd6162650
sorrycc <sorrycc@gmail.com>
2016-04-08 13:23:10 +0800

    update demo

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,14 +2,10 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { createSelector } from 'reselect';
 import Count from '../components/Count/Count';
-import { Link } from 'react-router';
 
 const App = (props) =>
   <div>
-    <h1>Demo</h1>
     <Count {...props} />
-    <br />
-    <Link to="/foo">Go To Foo</Link>
   </div>
 
 App.propTypes = {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,15 @@
 import React, { PropTypes } from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute, Link } from 'react-router';
 import App from '../containers/App';
+
+const Layout = ({ children }) =>
+  <div>
+    <h1>Demo</h1>
+    {children}
+    <br />
+    <Link to="/">Go To /</Link><br />
+    <Link to="/foo">Go To /foo</Link><br />
+  </div>
 
 const Foo = (props) =>
   <div>
@@ -9,8 +18,10 @@ const Foo = (props) =>
 
 const Routes = ({ history }) =>
   <Router history={history}>
-    <Route path="/" component={App} />
-    <Route path="/foo" component={Foo} />
+    <Route path="/" component={Layout}>
+      <IndexRoute component={App} />
+      <Route path="/foo" component={Foo} />
+    </Route>
   </Router>
 
 Routes.propTypes = {
